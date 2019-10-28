574f484bfe046293a3cea5006c749baf9c87aeb3
Joaquin Alberto Huauya Arontico <jhuauyaa@everis.com>
2019-10-28 18:03:18 -0500

    first commit

---
+++ src/main/java/com/joaquin/app/AppConfig.java
@@ -0,0 +1,22 @@
+package com.joaquin.app;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.reactive.function.client.WebClient;
+
+@Configuration
+public class AppConfig {
+
+	@Value("${config.base.endpoint}")
+	private String url;
+	
+	@Bean
+	@LoadBalanced
+	public WebClient.Builder registrarWebClient() {
+		return WebClient.builder().baseUrl(url);
+		
+	}
+	
+}
---
+++ src/main/java/com/joaquin/app/models/services/ProductoServiceImpl.java
@@ -0,0 +1,98 @@
+package com.joaquin.app.models.services;
+
+import static org.springframework.http.MediaType.*;
+
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.buffer.DataBuffer;
+import org.springframework.http.client.MultipartBodyBuilder;
+import org.springframework.http.codec.multipart.FilePart;
+import org.springframework.stereotype.Service;
+import org.springframework.web.reactive.function.client.WebClient;
+
+import com.joaquin.app.models.Producto;
+
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+
+@Service
+public class ProductoServiceImpl implements ProductoService {
+
+	@Autowired
+	private WebClient.Builder client;
+	
+	@Override
+	public Flux<Producto> findAll() {
+		
+		return client.build().get().accept(APPLICATION_JSON_UTF8)
+				.exchange()
+				.flatMapMany(response-> response.bodyToFlux(Producto.class));
+	}
+
+	@Override
+	public Mono<Producto> findById(String id) {
+		
+		Map<String, Object> params = new HashMap<String, Object>();
+		params.put("id", id);
+		
+		return client.build().get().uri("/{id}",params)
+				.accept(APPLICATION_JSON_UTF8)
+				//.exchange()
+				//.flatMap(response -> response.bodyToMono(Producto.class));
+				.retrieve()
+				.bodyToMono(Producto.class);
+	}
+
+	@Override
+	public Mono<Producto> save(Producto producto) {
+		
+		return client.build().post()
+				.accept(APPLICATION_JSON_UTF8)
+				.contentType(APPLICATION_JSON_UTF8)
+				//.body(fromObject(producto))
+				.syncBody(producto)
+				.retrieve()
+				.bodyToMono(Producto.class);
+	}
+
+	@Override
+	public Mono<Producto> update(Producto producto, String id) {
+		
+		return client.build().put()
+				.uri("/{id}",Collections.singletonMap("id", id))
+				.accept(APPLICATION_JSON_UTF8)
+				.contentType(APPLICATION_JSON_UTF8)
+				//.body(fromObject(producto))
+				.syncBody(producto)
+				.retrieve()
+				.bodyToMono(Producto.class);
+	}
+
+	@Override
+	public Mono<Void> delete(String id) {
+		
+		return client.build().delete().uri("/{id}",Collections.singletonMap("id", id))
+				.retrieve()
+				.bodyToMono(Void.class);
+	}
+
+	@Override
+	public Mono<Producto> upload(FilePart file, String id) {
+		MultipartBodyBuilder parts= new MultipartBodyBuilder();
+		parts.asyncPart("file", file.content(), DataBuffer.class)
+			.headers(h->{
+				h.setContentDispositionFormData("file", file.filename());
+			});
+		
+		return client.build().post()
+				.uri("/upload/{id}",Collections.singletonMap("id", id))
+				.contentType(MULTIPART_FORM_DATA)
+				.syncBody(parts.build())
+				.retrieve()
+				.bodyToMono(Producto.class);
+	}
+
+}
